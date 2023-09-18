bab88815b38d9f0b8d50753dc80a38670096d023
Vikhu <denergo.saberu@yandex.ru>
2023-09-18 20:23:11 +0300

    sql added

---
+++ SQL.py
@@ -0,0 +1,5 @@
+Задание 1
+SELECT c.login, o."inDelivery" FROM "Couriers" AS c JOIN "Orders" AS o ON o."courierId" = c.id WHERE o."inDelivery" = true;
+
+Задание 2
+SELECT track, CASE WHEN finished = true THEN 2 WHEN cancelled = true THEN -1 WHEN "inDelivery" = true THEN 1 ELSE 0 END AS status FROM "Orders";
